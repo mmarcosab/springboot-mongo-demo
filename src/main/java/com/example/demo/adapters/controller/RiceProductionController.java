b4247bbf9430e2a6632d1ad892fc0a0b478ce9ef
Marcos Vinicius <marcosab.vinicius@gmail.com>
2023-04-23 17:55:14 -0300

    add docker-compose and a crud with mongodb

--- src/main/java/com/example/demo/adapters/controller/RiceProductionController.java
+++ src/main/java/com/example/demo/adapters/controller/RiceProductionController.java
@@ -2,18 +2,39 @@ package com.example.demo.adapters.controller;
 
 import com.example.demo.adapters.database.document.RiceProductionData;
 import com.example.demo.adapters.database.repository.SpringDataRiceProductionRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Example;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/rice-production")
 public class RiceProductionController {
 
+    private final Logger logger = LoggerFactory.getLogger(RiceProductionController.class);
     private final SpringDataRiceProductionRepository springDataRiceProductionRepository;
 
     public RiceProductionController(final SpringDataRiceProductionRepository springDataRiceProductionRepository) {
         this.springDataRiceProductionRepository = springDataRiceProductionRepository;
+    }
+
+    @GetMapping("/error")
+    public ResponseEntity<?> createError() {
+        logger.error("log error");
+        return ResponseEntity.badRequest().build();
+    }
+
+    @GetMapping("/warn")
+    public ResponseEntity<?> createWarn() {
+        logger.warn("log warn");
+        return ResponseEntity.ok().build();
     }
 
     @PostMapping
@@ -23,31 +44,79 @@ public class RiceProductionController {
 
     @PutMapping("/{id}")
     public ResponseEntity<?> update(@PathVariable final String id, @RequestBody final RiceProductionData riceProductionData) {
-        var response = springDataRiceProductionRepository.findById(id);
+        final var response = springDataRiceProductionRepository.findById(id);
         if(response.isEmpty()) {
             return ResponseEntity.notFound().build();
         }
-        var responseUpdated = RiceProductionData.update(id, riceProductionData);
+        final var responseUpdated = RiceProductionData.update(id, riceProductionData);
         return ResponseEntity.ok(springDataRiceProductionRepository.save(responseUpdated));
     }
 
     @GetMapping
-    public ResponseEntity<?> listRiceproduction() {
-        return ResponseEntity.ok(springDataRiceProductionRepository.findAll());
+    public ResponseEntity<?> listRiceProduction(
+            @RequestParam(defaultValue = "0") final Integer pageNumber,
+            @RequestParam(defaultValue = "5") final Integer size
+    ) {
+        final var response = springDataRiceProductionRepository.findAll(PageRequest.of(pageNumber, size));
+        return ResponseEntity.ok(convertToResponse(response));
     }
 
     @GetMapping("/area/{countryName}")
-    public ResponseEntity<?> findByArea(@PathVariable final String countryName) {
-        var response = springDataRiceProductionRepository.findByArea(countryName);
+    public ResponseEntity<?> findByArea(@PathVariable final String countryName,
+                                        @RequestParam(defaultValue = "0") final Integer pageNumber,
+                                        @RequestParam(defaultValue = "5") final Integer size) {
+        final var response = springDataRiceProductionRepository.findByArea(countryName, PageRequest.of(pageNumber, size));
         if(response.isEmpty()) {
             return ResponseEntity.notFound().build();
         }
-        return ResponseEntity.ok(response);
+        return ResponseEntity.ok(convertToResponse(response));
+    }
+
+    @GetMapping("/value/{value}")
+    public ResponseEntity<?> listByValue(@RequestParam final String value,
+                                        @RequestParam(defaultValue = "higher") final String comparator,
+                                        @RequestParam(defaultValue = "0") final Integer pageNumber,
+                                        @RequestParam(defaultValue = "5") final Integer size) {
+        Page<RiceProductionData> response;
+        if ("higher".equals(comparator)) {
+            response = springDataRiceProductionRepository.listByValueEqualOrHigher(value, PageRequest.of(pageNumber, size));
+        } else if ("less".equals(comparator)) {
+            response = springDataRiceProductionRepository.listByValueEqualOrLess(value, PageRequest.of(pageNumber, size));
+        } else {
+            response = springDataRiceProductionRepository.findByValue(value, PageRequest.of(pageNumber, size));
+        }
+        return ResponseEntity.ok(convertToResponse(response));
+    }
+
+    @GetMapping("/areas")
+    public ResponseEntity<?> findByAreas(@RequestParam final String areaOne,
+                                         @RequestParam final String areaTwo,
+                                         @RequestParam(defaultValue = "0") final Integer pageNumber,
+                                         @RequestParam(defaultValue = "5") final Integer size) {
+        return ResponseEntity.ok(convertToResponse(springDataRiceProductionRepository.findByAreas(
+                areaOne, areaTwo, PageRequest.of(pageNumber, size)))
+        );
+    }
+
+    @GetMapping("/values")
+    public ResponseEntity<?> findByValues(@RequestParam final String beginValue,
+                                          @RequestParam final String endValue,
+                                          @RequestParam(defaultValue = "0") final Integer pageNumber,
+                                          @RequestParam(defaultValue = "5") final Integer size) {
+        return ResponseEntity.ok(convertToResponse(springDataRiceProductionRepository.findByValues(beginValue, endValue, PageRequest.of(pageNumber, size))));
+    }
+
+    @GetMapping("/years")
+    public ResponseEntity<?> findByPeriod(@RequestParam final String beginValue,
+                                          @RequestParam final String endValue,
+                                          @RequestParam(defaultValue = "0") final Integer pageNumber,
+                                          @RequestParam(defaultValue = "5") final Integer size) {
+        return ResponseEntity.ok(convertToResponse(springDataRiceProductionRepository.findByPeriod(beginValue, endValue, PageRequest.of(pageNumber, size))));
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<?> findById(@PathVariable final String id) {
-        var response = springDataRiceProductionRepository.findById(id);
+        final var response = springDataRiceProductionRepository.findById(id);
         if(response.isEmpty()) {
             return ResponseEntity.notFound().build();
         }
@@ -58,5 +127,14 @@ public class RiceProductionController {
     public ResponseEntity<?> deleteById(@PathVariable final String id) {
         springDataRiceProductionRepository.deleteById(id);
         return ResponseEntity.ok().build();
+    }
+
+    private Map<String, Object> convertToResponse(final Page<RiceProductionData> pagePersons) {
+        final Map<String, Object> response = new HashMap<>();
+        response.put("rice-productions", pagePersons.getContent());
+        response.put("current-page", pagePersons.getNumber());
+        response.put("total-items", pagePersons.getTotalElements());
+        response.put("total-pages", pagePersons.getTotalPages());
+        return response;
     }
 }
--- src/main/java/com/example/demo/adapters/database/repository/SpringDataRiceProductionRepository.java
+++ src/main/java/com/example/demo/adapters/database/repository/SpringDataRiceProductionRepository.java
@@ -1,12 +1,24 @@
 package com.example.demo.adapters.database.repository;
 
 import com.example.demo.adapters.database.document.RiceProductionData;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public interface SpringDataRiceProductionRepository extends MongoRepository<RiceProductionData, String> {
-    List<RiceProductionData> findByArea(final String countryName);
+    Page<RiceProductionData> findByArea(final String area, final Pageable pageable);
+    @Query("{ 'value' : { $gte: ?0 } }")
+    Page<RiceProductionData> listByValueEqualOrHigher(final String value, final Pageable pageable);
+    @Query("{ 'value' : {$lte: ?0 } }")
+    Page<RiceProductionData> listByValueEqualOrLess(final String value, final Pageable pageable);
+    Page<RiceProductionData> findByValue(final String value, final Pageable pageable);
+    @Query("{$or:[{'area': ?0}, {'area': ?1 }]}")
+    Page<RiceProductionData> findByAreas(final String areaOne, final String areaTwo, final Pageable pageable);
+    @Query("{'value': {$in: [ ?0,  ?1 ]}}")
+    Page<RiceProductionData> findByValues(final String valueBegin, final String valueEnd, final Pageable pageable);
+    @Query("{'year': {$in: [ ?0,  ?1 ]}}")
+    Page<RiceProductionData> findByPeriod(final String yearBegin, final String yearEnd, final Pageable pageable);
 }
